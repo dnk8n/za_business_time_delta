9c73ad3d163c9dbd71dc5c702a47f1b596f0814d
Dean Kayton <deankayton@gmail.com>
2020-02-03 20:22:58 +0200

    Tidy up basic helper

--- api/index.py
+++ api/index.py
@@ -35,20 +35,38 @@ def home():
         <html>
         <body>
         
-        <h2>Welcome to basic request helper.</h2>
-        <p>This form will be submitted using the GET method to /time_delta endpoint</p>
+        <h1>Welcome to a very basic request helper for the <b>/time_delta</b> endpoint</h1> 
+        <p>This form will be submitted using the <b>GET</b> method to the <b>/time_delta</b> endpoint</p>
         
         <form action="/time_delta" method="GET">
           Start time:<br>
-          <input type="text" name="start_time" value="2019-01-01T00:00:00.000000+02:00">
+          <input type="text" name="start_time" value="2019-01-01T00:00:00">
           <br>
           End time:<br>
-          <input type="text" name="end_time" value="2021-01-01T00:00:00.000000+02:00">
+          <input type="text" name="end_time" value="2021-01-01T00:00:00">
           <br><br>
           <input type="submit" value="Submit">
         </form>
         
         <p>After you submit, the form values are visible in the address bar of the current browser tab.</p>
+
+        <hr>
+
+        <h3>Description:</h3>
+        <p>
+        An API end point that will calculate the total number of South African business seconds between two
+        <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a> formatted time strings.
+        </p> 
+
+        <h3>Brief:</h3>
+        <p>
+        Provide an API end point that will calculate the total number of business seconds between two given
+        times. A business second is defined as any whole second that elapses after 08:00 and before 17:00 during a
+        weekday (Monday - Friday) that is not a public holiday in the Republic of South Africa. The end point must
+        support only list GET requests and must take two parameters: start_time and end_time. Parameter values will be
+        in ISO-8601 format. You are guaranteed that start_time will be before end_time. The end point must respond with
+        only a single integer value for successful requests or a suitable error message string for failed requests.
+        </p>
         
         </body>
         </html>
